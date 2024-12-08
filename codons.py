a561c4e06cfd541ba587504eb64e207f96f31f9a
MAYARDAHER <MAYARDA23@GMAIL.COM>
2024-12-08 12:55:06 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,7 @@
 def create_codon_dict(file_path):
+   path = "data/codons.txt"
+   with open(path, "r") as file:
+      rows = file.readlines()
    dic = {}
    for row in rows:
      cells = row.strip().split('\t')
